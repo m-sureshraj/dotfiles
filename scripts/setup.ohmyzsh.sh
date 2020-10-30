41ef80cc9caeb1a5208cde70f21945136498f5ac
m-sureshraj <m.s.suresh100@gmail.com>
2020-10-30 21:49:17 +0530

    feat: change shell to zsh

--- scripts/setup.ohmyzsh.sh
+++ scripts/setup.ohmyzsh.sh
@@ -22,3 +22,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions "$PLUGINS_PATH"/zsh-a
 
 print "Downloading Cobalt2 theme"
 wget https://raw.githubusercontent.com/wesbos/Cobalt2-iterm/master/cobalt2.zsh-theme -P "$OH_MY_ZSH_PATH"/themes
+
+print "Changing shell to zsh"
+chsh -s "$(command -v zsh)"
